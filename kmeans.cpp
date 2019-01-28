372c8b9e94710b6a93ff24ec5acba12c14ebd9ab
Samarth Aggarwal <samarthaggarwal2510@gmail.com>
2019-01-28 18:18:26 +0530

    assigning means to points

--- kmeans.cpp
+++ kmeans.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 #include<stdlib.h>
 #include<time.h>
+#include<climits>
 using namespace std;
+
+void assign(int n, int k, int points[][4], float means[][3]){ // assigns means to points as 4th dimension
+	
+	float temp, tempDist, minDist=INT_MAX, minDistIndex=-1;
+	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<k;j++){
+
+			tempDist=0;
+			for(int k=0;k<3;k++){
+				temp=points[i][k]-means[j][k];
+				tempDist += temp*temp;
+			}
+
+			if(tempDist<minDist){
+				minDist=tempDist;
+				minDistIndex=j;
+			}
+		}
+
+		points[i][3]=minDistIndex;
+	}
+
+	return;
+}
 
 int main(int argc, char *argv[]){
 	srand (time(NULL));
@@ -12,7 +38,7 @@ int main(int argc, char *argv[]){
 	
 	cout<<"Enter number of points\n";
 	cin>>n;
-	int points[n][3];
+	int points[n][4];
 	float means[k][3];
 
 	for(int i=0;i<n;i++){
@@ -23,13 +49,15 @@ int main(int argc, char *argv[]){
 
 	for(int i=0; i<k; i++){
 		for(int j=0;j<3;j++){
-			means[i][j]=rand()%100;
+			means[i][j]=rand()%50;
 		}
 	}
 
+	assign(n,k,points,means);
+
 	cout<<"points\n";
 	for(int i=0;i<n;i++){
-		for(int j=0;j<3;j++){
+		for(int j=0;j<4;j++){
 			cout<<points[i][j]<<" ";
 		}
 		cout<<endl;
